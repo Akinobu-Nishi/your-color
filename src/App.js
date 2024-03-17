07f2f0be68dbc3300b7e0b723b1be08bcaeae7a7
Akinobu-Nishi <24akinobu@gmail.com>
2024-03-17 19:16:21 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello fron V2</h1>
+        <h1>Your Color</h1>
       </header>
     </div>
   );
